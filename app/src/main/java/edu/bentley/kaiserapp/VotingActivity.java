f7538c2e2f266d95a5bb0eeb266efc88d1d5b536
Kleppinger <KLEPPIN_JONA@bentley.edu>
2016-04-24 23:02:04 -0400

    Maybe a Working Voting System

--- app/src/main/java/edu/bentley/kaiserapp/VotingActivity.java
+++ app/src/main/java/edu/bentley/kaiserapp/VotingActivity.java
@@ -1,11 +1,24 @@
 package edu.bentley.kaiserapp;
 
 import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Calendar;
 
 public class VotingActivity extends AppCompatActivity {
 
@@ -15,7 +28,16 @@ public class VotingActivity extends AppCompatActivity {
     private View.OnClickListener votingListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
+            Thread t = new Thread(voteTask);
+            t.start();
+        }
+    };
 
+    private Handler handler = new Handler() {
+        public void handleMessage(Message message) {
+            boolean flag = (Boolean)message.obj;
+            if(flag)
+                startActivity(new Intent(getApplicationContext(), VotingErrorActivity.class));
         }
     };
 
@@ -35,4 +57,49 @@ public class VotingActivity extends AppCompatActivity {
         tv3 = (TextView)findViewById(R.id.voting_tv3);
         tv3.setOnClickListener(votingListener);
     }
+
+    private Runnable voteTask = new Runnable() {
+        public void run(){
+            String URL = "jdbc:mysql://frodo.bentley.edu:3306/cs480icecream";
+            String username = "jkleppinger";
+            String password = "icecream";
+
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+            } catch (ClassNotFoundException e) {
+                Log.e("JDBC", "Did not load driver");
+            }
+
+            Statement stmt;
+            Connection con;
+            try {
+                con = DriverManager.getConnection (URL, username, password);
+
+                PreparedStatement preStmt = con.prepareStatement("SELECT phone_number, timestamp FROM tblVote" +
+                        "WHERE phone_number = ?");
+                preStmt.setString(1, phoneNumber);
+
+                ResultSet result = preStmt.executeQuery();
+
+                Calendar c = Calendar.getInstance();
+                boolean flag = true;
+                while (result.next()) {
+                    Date timestamp = result.getDate(2);
+                    Log.e("Voting", timestamp.toString());
+                    if (timestamp.getMonth() == c.get(Calendar.MONTH)) {
+                        flag = false;
+                        break;
+                    }
+                }
+
+                Message msg = new Message();
+                msg.obj = flag;
+                handler.sendMessage(msg);
+                con.close();
+            }
+            catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    };
 }
---
+++ app/src/main/java/edu/bentley/kaiserapp/VotingErrorActivity.java
@@ -0,0 +1,23 @@
+package edu.bentley.kaiserapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class VotingErrorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_voting_error);
+
+        findViewById(R.id.btn_voting_home).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), ContactActivity.class));
+            }
+        });
+    }
+}
